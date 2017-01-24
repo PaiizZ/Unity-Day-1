889bdb6866eb350c0f8be9704459770070f9d239
paiizz <wanchanapon.t@ku.th>
2017-01-24 12:17:15 +0700

    new screen and move cube

---
+++ Assets/SpaceshipController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceshipController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis("Horizontal") != 0 ) {
+			this.gameObject.transform.Translate (Input.GetAxis ("Horizontal") * Vector3.right);
+		} else if (Input.GetAxis("Vertical") != 0) {
+			this.gameObject.transform.Translate (Input.GetAxis ("Vertical") * Vector3.up);
+		} 
+	}
+
+}
