03433476464571d054f78f9c8570a40b37309f6c
paiizz <wanchanapon.t@ku.th>
2017-02-20 22:47:48 +0700

    :bug: text

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,39 +4,32 @@ using UnityEngine.UI;
 
 public class EnemyController : MonoBehaviour {
 	public AudioClip myExplostionBoom;
-	public bool restart;
-	public Text gameOverText;
+	public SpaceshipController spaceship;
+//	public Text gameOverText;
+
 	// Use this for initialization
 	void Start () {
-		this.restart = false ;
-		gameOverText = GetComponent<UnityEngine.UI.Text>();
-
+//		gameOverText = GetComponent<Text>();
+		spaceship = SpaceshipController.instance;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		this.gameObject.transform.Translate (Vector3.down * Time.deltaTime * 5f);
-		if (restart) {
-			if (Input.GetKeyDown (KeyCode.R))
-			{
-				gameOverText.text = "";
-				Application.LoadLevel (Application.loadedLevel);
-			}
-		}
+
 	}
 
 	void OnTriggerEnter(Collider other){
 		if (other.gameObject.tag == "Player") {
 			AudioSource.PlayClipAtPoint (myExplostionBoom, gameObject.transform.position);
+//			Debug.Log (other.gameObject.GetComponent<SpaceshipController> ().restart);
+			spaceship.setRestart (true);
+//			 Debug.Log (other.gameObject.GetComponent<SpaceshipController> ().restart);
 			Destroy (other.gameObject);
-			this.restart = true;
-			GetComponent<UnityEngine.UI.Text>().text = "Game Over";
+			
 //			this.gameObject.GetComponent<AudioSource> ().Play ();
 		}
 			
 	}
-
-	bool getRestart(){
-		return this.restart;
-	}
+		
 }
--- Assets/Scripts/SpaceshipController.cs
+++ Assets/Scripts/SpaceshipController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class SpaceshipController : MonoBehaviour {
 	[System.Serializable]
@@ -7,25 +8,40 @@ public class SpaceshipController : MonoBehaviour {
 		public float xMin , xMax ,yMin, yMax;
 	}
 	public GameObject myBullet;
+	public static SpaceshipController instance;
 	// Use this for initialization
 	public float speed;
+	public Text gameOverText;
 	public Boundary boundary;
+	public bool restart;
 	void Start () {
-	
+//		gameOverText = GetComponent<Text>();
+		if (instance==null) {
+			instance = this;
+		} else if (instance != this) {
+			Destroy (gameObject);
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-//		if (Input.GetAxis("Horizontal") != 0 ) {
-//			this.gameObject.transform.Translate (Input.GetAxis ("Horizontal") * Vector3.right);
-//		} else if (Input.GetAxis("Vertical") != 0) {
-//			this.gameObject.transform.Translate (Input.GetAxis ("Vertical") * Vector3.up);
-//		} 
 
 		if (Input.GetKeyDown(KeyCode.Space)) {
 			Instantiate (myBullet, this.gameObject.transform.position,Quaternion.identity);
 		}
+		if (restart) {
+			Debug.Log ("eiei restart");
+			gameOverText.text = "Game Over";
+			if (Input.GetKeyDown (KeyCode.R))
+			{
+				Debug.Log ("eiei");
+				gameOverText.text = "kjkjjkk";
+				Application.LoadLevel (Application.loadedLevel);
+				this.restart = false;
+			}
+		}
 	}
+
 	void FixedUpdate(){
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
@@ -41,5 +57,8 @@ public class SpaceshipController : MonoBehaviour {
 			
 	}
 
-
+	public void setRestart(bool re){
+		this.restart = re;
+	}
+		
 }
